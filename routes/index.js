1296ac6fa7406e5e81c48a4b548ffeef88266205
jkinisnger <joy.kinsinger02@gmail.com>
2022-03-08 06:46:40 -0600

    checking an array and using req.bodu with Postman

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,58 @@ var router = express.Router();
 //   res.render('index', { title: 'Express' });
 // });
 
-router.post('/', function(req, res){
-  res.send('You successfulyl created a POST route!')
+// practice HTTP Verbs w/ Postman
+// router.post('/', function(req, res){
+//   res.send('You successfulyl created a POST route!')
+// });
+
+// router.get('/', function(req, res){
+//   res.send('You successfully created a GET route!')
+// });
+
+// router.put('/', function(req, res){
+//   res.send('You successfully created a PUT route!')
+// });
+
+// router.delete('/', function (req, res){
+//   res.send('You successfully created a DELETE route')
+// });
+
+// defining parameters in a URL w/ Postman
+
+// router.get ('/', function (req, res){
+//   let queryName = req.query.name;
+//   if (queryName) {
+//     res.send('Welcome to the page ' + queryName )
+//   } else {
+//     res.send('Sad, no one is here!');
+//   }
+// })
+
+//checking against an array w/ Postman
+
+let animals = ['pig', 'goat', 'dog', 'cat']
+
+router.get ('/', function (req, res, next){
+  let queryAnimal = req.query.animal;
+
+  if(animals.includes(queryAnimal)) {
+    res.send('Yep, we have a ' + queryAnimal + '.');
+  } else {
+    res.send('Nope, no ' + queryAnimal + ' here.')
+  }
 });
 
-router.get('/', function(req, res){
-  res.send('You successfully created a GET route!')
-});
+//using req.body w/ Postman
 
-router.put('/', function(req, res){
-  res.send('You successfully created a PUT route!')
-});
+router.post('/', function(req, res, next){
+  let bodyAnimal = req.body;
 
-router.delete('/', function (req, res){
-  res.send('You successfully created a DELETE route')
+  if(animals.includes(bodyAnimal.animal)) {
+    res.send('Already have a ' + bodyAnimal.animal + ' thanks.'); 
+  } else {
+    animals.push(bodyAnimal.animal);
+    res.send(animals);
+  }
 });
-
 module.exports = router;
